20028d26d8082124b8a32898e7472704db0927d5
Athiya <athiyaez2007@gmail.com>
2026-05-02 23:29:31 +0700

    Reformat Pegawai.java with consistent package structure

--- Pegawai.java
+++ Pegawai.java
@@ -1,6 +1,5 @@
 package LK08;
 
-// Pegawai.java
 import java.util.Objects;
 
 public class Pegawai extends Person {
@@ -20,7 +19,6 @@ public class Pegawai extends Person {
         return super.toFileString() + ";" + tglLahir;
     }
 
-    // Digunakan oleh FileManager untuk memuat data dari file
     public static Pegawai fromFileString(String line) {
         String[] parts = line.split(";", 3);
         if (parts.length < 3)
